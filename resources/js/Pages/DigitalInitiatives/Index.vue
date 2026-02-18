782c2464bd5016b10202f98bd31d2e1ceb5cd54b
jethroGIT <jethro.andersson02@gmail.com>
2026-02-18 12:33:15 +0700

    fix: hapus action edit di digital initiative

--- resources/js/Pages/DigitalInitiatives/Index.vue
+++ resources/js/Pages/DigitalInitiatives/Index.vue
@@ -93,15 +93,6 @@
                                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
                                             </svg>
                                         </Link>
-                                        <Link
-                                            :href="`/digital-initiatives/${item.id}/edit`"
-                                            class="rounded-lg p-1.5 text-slate-400 transition-colors hover:bg-slate-100 hover:text-amber-600 dark:text-slate-500 dark:hover:bg-white/5 dark:hover:text-amber-400"
-                                            title="Edit"
-                                        >
-                                            <svg class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
-                                            </svg>
-                                        </Link>
                                     </div>
                                 </td>
                             </tr>
