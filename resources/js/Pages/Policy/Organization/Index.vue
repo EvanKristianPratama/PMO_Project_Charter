65b1a75761afc4f0b6a69d22fbd99c80d76310a5
jethroGIT <jethro.andersson02@gmail.com>
2026-06-03 12:12:35 +0700

    feat: staging dev

--- resources/js/Pages/Policy/Organization/Index.vue
+++ resources/js/Pages/Policy/Organization/Index.vue
@@ -4,7 +4,7 @@
             <!-- Diagram Card -->
             <div class="relative overflow-x-auto rounded-2xl border border-slate-200 bg-slate-50 p-6 dark:border-white/10 dark:bg-[#0f0f0f]">
                 <div class="min-w-[768px] overflow-hidden">
-                    <ThreeView :organization-structure-rows="organizationStructureRows" />
+                    <EITOrganization :organization-structure-rows="organizationStructureRows" />
                 </div>
             </div>
         </div>
@@ -13,7 +13,7 @@
 
 <script setup>
 import UserLayout from '@/Layouts/UserLayout.vue';
-import ThreeView from '@/Components/Architecture/Organization/ThreeView.vue';
+import EITOrganization from '@/Components/ITOperatingModel/Organization/EITOrganization.vue';
 
 const props = defineProps({
     organizationStructureRows: {
